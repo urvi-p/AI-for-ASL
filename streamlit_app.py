adde544a2b00b8369dca5404c752213350282dc5
avj99 <avnishjadhav091@gmail.com>
2021-10-23 16:49:51 -0600

    sidebar added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.header("AI For ASL")
 
-sidebar = st.sidebar.selectbox("Index",["Introduction"])
+sidebar = st.sidebar.selectbox(st.header("Index"),["Introduction"])
 
 
 
