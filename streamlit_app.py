b4d1b60302bb0b305b0854f60a9122e572eac6ac
avj99 <avnishjadhav091@gmail.com>
2021-10-23 18:01:24 -0600

    sidebar added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st 
 
-st.header("AI For ASL")
+st.title("AI For ASL")
 
 sidebar = st.sidebar.selectbox("Index",["About"])
 
 if sidebar == "About":
-    st.title("Problem")
+    st.header("Problem")
     with st.container():
         st.text_area("There are thousands of people in Canada who use sign languages as their primary form of communication. Although there are various resources to learn sign languages such as with courses or videos, these methods take extensive amounts of time to learn and become conversational in the language. Spoken languages have electronic translators which are easily accessible, simple to use, and allow for quick and immediate translations between numerous languages. This technology, however, does not accommodate users of sign languages and those wishing to communicate with someone using sign language.Our team would like to bridge this gap by creating a sign language translator, specifically for American Sign Language (ASL). The translator would allow for instant communication between a user of ASL and a non-user, removing the need to spend considerable amounts of time to become conversational in ASL. As an added benefit, the translator would also be a valuable resource for those learning ASL.")
         st.subheader("Contributors")
